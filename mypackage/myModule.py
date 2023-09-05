2bd94eca4b563307a7f412686886b5a1b86997a3
PercyMmutle <129300307+PercyMmutle@users.noreply.github.com>
2023-09-05 15:36:56 +0200

    First Commit

--- mypackage/myModule.py
+++ mypackage/myModule.py
@@ -13,7 +13,14 @@ def top_n(items, n):
        (3, 4, 9)
     '''
 
-    for i in items:
-        t = 0
-        Array_= []
-        if items[t] => i: 
+    for i in range(n):  # Keep sorting until we have the top n items
+        for j in range(len(items)-1-i):
+
+            if items[j] > items[j+1]:  # If this item is bigger than next the item..
+                items[j], items[j+1] = items[j+1], items[j]  # swap the two!
+                
+    # Get last two items
+    top_n = items[-n:]
+    
+    # Return in descending order
+    return top_n[::-1]
